82bc0760cb72e613a6df8d06718415e606e8337d
Vladislav Sena <vladislavsena@10-20-51-147.0200.fed.it.iosda.org>
2018-10-16 10:55:47 +0200

    ViewController to Opening(With entry)

---
+++ Music/Music/Opening.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Music
+//
+//  Created by Vladislav Sena on 15/10/2018.
+//  Copyright © 2018 VladislavSena. All rights reserved.
+//
+
+import UIKit
+
+class Opening: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+}
+
