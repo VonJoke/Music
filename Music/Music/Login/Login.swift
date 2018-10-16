79d461cb6fd41140eb729e3500869532da9ca635
Quentin Rolland <quentin.rolland@yahoo.fr>
2018-10-16 18:22:17 +0200

    Login screen design implementation

---
+++ Music/Music/Login/Login.swift
@@ -0,0 +1,37 @@
+//
+//  Login.swift
+//  Music
+//
+//  Created by Quentin Rolland on 16/10/2018.
+//  Copyright © 2018 VladislavSena. All rights reserved.
+//
+
+import UIKit
+
+class Login: UIViewController {
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBAction func accessButtonPressed(_ sender: Any) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
