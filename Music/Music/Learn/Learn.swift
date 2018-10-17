923762c226cffac14eab69540b93ee7dcee5d96f
Florin Aparaschivei <florinaparaschivei@10-20-51-192.0200.fed.it.iosda.org>
2018-10-17 10:14:43 +0200

    dad

---
+++ Music/Music/Learn/Learn.swift
@@ -0,0 +1,19 @@
+//
+//  Learn.swift
+//  Music
+//
+//  Created by Florin Aparaschivei on 17/10/2018.
+//  Copyright © 2018 VladislavSena. All rights reserved.
+//
+import Foundation
+import UIKit
+
+class LearnController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
