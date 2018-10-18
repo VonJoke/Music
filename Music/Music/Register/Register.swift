83b65050a6185869a3a544fbc62215dc11fbd9aa
Francesca Pellegrino <francescapellegrino@macbook-pro-di-francesca-2.local>
2018-10-18 09:43:11 +0200

    Register with file

---
+++ Music/Music/Register/Register.swift
@@ -0,0 +1,19 @@
+//
+//  Register.swift
+//  Music
+//
+//  Created by Francesca Pellegrino on 18/10/2018.
+//  Copyright © 2018 VladislavSena. All rights reserved.
+//
+
+import UIKit
+
+class Register: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
