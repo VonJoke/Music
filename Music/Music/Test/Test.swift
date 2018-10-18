fe53d62bf8b0db08cf0376625fa2b5307590e031
Quentin Rolland <quentin.rolland@yahoo.fr>
2018-10-18 14:22:54 +0200

    Test screen design

---
+++ Music/Music/Test/Test.swift
@@ -0,0 +1,30 @@
+//
+//  Test.swift
+//  Music
+//
+//  Created by Quentin Rolland on 18/10/2018.
+//  Copyright © 2018 VladislavSena. All rights reserved.
+//
+
+import UIKit
+
+class Test: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
