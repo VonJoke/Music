c71d1658f8defe1f3c7130896bd17c34a92106c4
Vladislav Sena <vladislavsena@10-20-51-147.0200.fed.it.iosda.org>
2018-10-18 11:27:56 +0200

    Quiz (sound) added

--- Music/Music/Quiz/Quiz.swift
+++ Music/Music/Quiz/Quiz.swift
@@ -7,13 +7,39 @@
 //
 
 import UIKit
+import AVFoundation
 
 class Quiz: UIViewController {
 
+    var audioPlayer = AVAudioPlayer()
+    @IBOutlet weak var playBtn: UIButton!
+    
+    @IBAction func playSound(_ sender: Any) {
+        playBtn.transform = CGAffineTransform(scaleX: 0.9, y: 0.9)
+        
+        UIView.animate(withDuration: 1.0,
+                       delay: 0,
+                       usingSpringWithDamping: 0.3,
+                       initialSpringVelocity: 5.0,
+                       options: .allowUserInteraction,
+                       animations: { [weak self] in
+                        self?.playBtn.transform = .identity
+            },
+                       completion: nil)
+        
+        audioPlayer.play()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let sound = Bundle.main.path(forResource: "C(Stretched)", ofType: "wav")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: sound!))
+        } catch {
+            print(error)
+        }
     }
     
 
