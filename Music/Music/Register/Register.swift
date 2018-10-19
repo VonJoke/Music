021d43e34924caa8e578c50f4f6ef5fce36574af
Francesca Pellegrino <francescapellegrino@10-20-51-161.0200.fed.it.iosda.org>
2018-10-19 12:02:06 +0200

    Register picker big update

--- Music/Music/Register/Register.swift
+++ Music/Music/Register/Register.swift
@@ -10,13 +10,21 @@ import UIKit
 
 class Register: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var instrumentText: UITextField!
+    
     let pickerInstruments = ["Piano","Violin","Bass","Guitar"]
+    var selected: String?
+    let picker = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        createPicker()
+    }
+    
+    func createPicker() {
+        picker.delegate = self
         
-        //        pickerView.delegate = self
-        //        pickerView.dataSource = self
+        instrumentText.inputView = picker
     }
     
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
@@ -30,4 +38,12 @@ class Register: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
     func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return pickerInstruments.count
     }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        selected = pickerInstruments[row]
+        instrumentText.text = selected
+        
+        self.view.endEditing(true)
+    }
 }
+
