680f1a91a881c3a3389ef69a5248c2c38464c0dd
Quentin Rolland <quentin.rolland@yahoo.fr>
2018-10-22 09:50:35 +0200

    Learn screen update with one cell

--- Music/Music/Learn/Learn.swift
+++ Music/Music/Learn/Learn.swift
@@ -8,12 +8,33 @@
 import Foundation
 import UIKit
 
-class Learn: UIViewController {
+class Learn: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var ui_tableview: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         // Do any additional setup after loading the view, typically from a nib.
+        ui_tableview.dataSource = self // Tells the tableview that the object Learn is its data source
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell :UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "test-cell", for: indexPath)
+        
+        if let titleLabel = cell.textLabel {
+            titleLabel.text = "Music theory"
+        }
+        
+        if let detailLabel = cell.detailTextLabel {
+            detailLabel.text = "Theory purpose"
+        }
+        return cell
+    }
+    
+    
+
 }
